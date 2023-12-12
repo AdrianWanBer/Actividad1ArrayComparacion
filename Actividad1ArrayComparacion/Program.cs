cadc848cc607b97e605c0c8753c220aada2251ac
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2023-12-12 14:11:59 +0100

    Agregar archivos de proyecto.

---
+++ Actividad1ArrayComparacion/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Actividad1ArrayComparacion
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            char[] Vocales1 = {'a', 'e', 'i', 'o','u' };
+            char[] Vocales2 = { 'a', 'e', 'i', 'o', 'x' };
+
+            for(int recorrido = 0; recorrido < Vocales1.Length; recorrido++)
+            {
+               if( Vocales1[recorrido] != Vocales2[recorrido])
+                {
+                    Console.WriteLine("En la posicion: " + recorrido + " del segundo array esta mal");
+                }
+            }
+        }
+    }
+}
